5211fe35e212042aede60c9944ed89d4488a2e2e
Atr <literal@mvs-macbook-pro.local>
2018-01-29 20:09:35 +0100

    Initial Commit

---
+++ LyricsViewer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LyricsViewer
+//
+//  Created by MV on 29/01/2018.
+//  Copyright © 2018 velvet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
